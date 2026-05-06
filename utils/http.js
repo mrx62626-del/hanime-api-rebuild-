a8840d7380c084b8837746602d8bdee6f4d0fe36
tanbirst1st1 <senpaiorbit@gmail.com>
2026-05-06 21:08:46 +0530

    Update http.js

--- utils/http.js
+++ utils/http.js
@@ -1,3 +1,4 @@
+// utils/http.js
 import axios from 'axios';
 import { config } from '../core/config.js';
 
@@ -6,7 +7,21 @@ const client = axios.create({
   headers: config.headers,
 });
 
+// Separate client for anikotoapi.site (JSON API)
+const apiClient = axios.create({
+  timeout: config.timeout,
+  headers: {
+    ...config.headers,
+    'Accept': 'application/json',
+    'Origin': 'https://anikototv.to',
+    'Referer': 'https://anikototv.to/',
+  },
+});
+
 export async function get(url, options = {}) {
-  const res = await client.get(url, options);
+  // Use different client based on URL
+  const isApiSite = url.includes('anikotoapi.site');
+  const clientToUse = isApiSite ? apiClient : client;
+  const res = await clientToUse.get(url, options);
   return res.data;
 }
