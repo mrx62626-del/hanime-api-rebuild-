cfb268b1121c54fd8015994d2a146e7e16f25468
tanbirst1st1 <senpaiorbit@gmail.com>
2026-05-06 21:36:22 +0530

    Update http.js

--- utils/http.js
+++ utils/http.js
@@ -1,4 +1,3 @@
-// utils/http.js
 import axios from 'axios';
 import { config } from '../core/config.js';
 
@@ -7,7 +6,6 @@ const client = axios.create({
   headers: config.headers,
 });
 
-// Separate client for anikotoapi.site (JSON API)
 const apiClient = axios.create({
   timeout: config.timeout,
   headers: {
@@ -19,14 +17,12 @@ const apiClient = axios.create({
 export async function get(url, options = {}) {
   try {
     const isApiSite = url.includes('anikotoapi.site');
-    const clientToUse = isApiSite ? apiClient : client;
-    const res = await clientToUse.get(url, options);
+    const useClient = isApiSite ? apiClient : client;
+    const res = await useClient.get(url, options);
     return res.data;
   } catch (error) {
     if (error.response) {
-      throw new Error(
-        `Request failed with status code ${error.response.status} for URL: ${url}`
-      );
+      throw new Error(`Request failed with status code ${error.response.status} for ${url}`);
     }
     throw error;
   }
